2c97376da78c881d6885c50205869ae0aae8b8ba
Rukayat Balogun <74791083+rukayat-balogun@users.noreply.github.com>
2021-05-04 14:27:43 +0100

    Add files via upload

---
+++ password_detection.py
@@ -0,0 +1,17 @@
+import subprocess
+
+data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+
+profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+
+for i in profiles:
+   
+    password= subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i,
+                        'key=clear']).decode('utf-8').split('\n')
+    
+    password = [b.split(":")[1][1:-1] for b in password if "Key Content" in b]
+
+    try:
+        print ("{:<30}|  {:<}".format(i, password[0]))
+    except IndexError:
+        print ("{:<30}|  {:<}".format(i, ""))
